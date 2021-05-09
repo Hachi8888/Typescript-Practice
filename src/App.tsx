5c4d964461f204a85c242773db7ac97cbb3f7ca8
hachi8888 <53389277+Hachi8888@users.noreply.github.com>
2021-05-09 21:01:10 +0900

    Optional chaining

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import "./styles.css";
 import { Todo } from "./todo";
 import { todoType } from "./types/todo";
 import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "はち",
+  hobbies: ["映画", "散歩"]
+};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<todoType>>([]);
@@ -16,6 +23,7 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color={"red"} fontSize={"18px"} />
       <button onClick={onClickFetchData}>ボタン</button>
       {todos.map((todo) => (
