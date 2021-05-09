612b898709c5cc8b6f6d4b92630b8bf69170f446
hachi8888 <53389277+Hachi8888@users.noreply.github.com>
2021-05-09 20:35:12 +0900

    use "Omit"

--- src/todo.tsx
+++ src/todo.tsx
@@ -1,8 +1,6 @@
 import { todoType } from "./types/todo";
 
-export const Todo = (
-  props: Pick<todoType, "userId" | "title" | "completed">
-) => {
+export const Todo = (props: Omit<todoType, "id">) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "完" : "未";
   return <p>{`[${completeMark}] ${title}(ユーザー:${userId})`}</p>;
